56e2dfc072d5a356364962041a8020e5786c8213
Kayla Ward <kaylaw0127@gmail.com>
2020-01-31 20:47:52 -0800

    made it so you can type in description box

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
+//description box
+
+ $("#setText").click(function(event) { 
+     $('#description-9a').val(); 
+}); 
+
+
+
+
 // save button
 $("#saveBtn").on("click", function() {
     alert("I've been clicked!")
 })
-
-
-
-
-
-
-console.log('this')
-
-
 
 })
