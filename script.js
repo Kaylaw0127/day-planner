563ff483399130d24e92eaf5c6c914bd8d24ea84
Kayla Ward <kaylaw0127@gmail.com>
2020-02-04 14:15:17 -0800

    created color change for each hr based off current time

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// display time
-var now = moment().format("dddd, MMMM Do YYYY, h:mm a")
-console.log(now)
-
-$("#currentDay").html(now)
-
-//color depending on time
-if (moment().format() < 9) {
-    nineInfo.classList.add('future')
-    } else if (moment().format() == 9) {
-    nineInfo.classList.add('present')
-        } else if (moment().format() > 9) {
-    nineInfo.classList.add('past')
-
-    // isAfter or isBefore?
-    //isSame  or .diff
 
 //description box var set
 var nineInfo = document.getElementById("description-9")
@@ -36,8 +20,100 @@ var save2 = document.getElementById("2")
 var save3 = document.getElementById("3") 
 var save4 = document.getElementById("4")
 var save5 = document.getElementById("5")  
+
+// var for color change
+var now = moment().format("H")
+
+//display time on page
+var display = moment().format('dddd, MMMM Do YYYY, h:mm a')
+
+$("#currentDay").html(display)
+
+///// COLOR DEPENDING ON TIME//////
+// 9 am
+if (now < 9) {
+    nineInfo.style.backgroundColor = "green"
+    } else if (now == 9) {
+    nineInfo.style.backgroundColor = "red"
+        } else {
+    nineInfo.style.backgroundColor = "gray"
+}
+
+// 10 am
+if (now < 10) {
+    tenInfo.style.backgroundColor = "green"
+    } else if (now == 10) {
+    tenInfo.style.backgroundColor = "red"
+        } else {
+    tenInfo.style.backgroundColor = "gray"
+}
+
+// 11 am
+if (now < 11) {
+    elevenInfo.style.backgroundColor = "green"
+    } else if (now == 11) {
+    elevenInfo.style.backgroundColor = "red"
+        } else {
+    elevenInfo.style.backgroundColor = "gray"
+}
+
+// 12 pm
+if (now < 12) {
+    twelveInfo.style.backgroundColor = "green"
+    } else if (now == 12) {
+    twelveInfo.style.backgroundColor = "red"
+        } else {
+    twelveInfo.style.backgroundColor = "gray"
+}
+
+// 1 pm
+if (now < 13) {
+    oneInfo.style.backgroundColor = "green"
+    } else if (now == 13) {
+    oneInfo.style.backgroundColor = "red"
+        } else {
+    oneInfo.style.backgroundColor = "gray"
+}
+
+// 2 pm
+if (now < 14) {
+    twoInfo.style.backgroundColor = "green"
+    } else if (now == 14) {
+    twoInfo.style.backgroundColor = "red"
+        } else {
+    twoInfo.style.backgroundColor = "gray"
+}
+
+// 3 pm
+if (now < 15) {
+    threeInfo.style.backgroundColor = "green"
+    } else if (now == 15) {
+    threeInfo.style.backgroundColor = "red"
+        } else {
+    threeInfo.style.backgroundColor = "gray"
+}
+
+// 4 pm
+if (now < 16) {
+    fourInfo.style.backgroundColor = "green"
+    } else if (now == 16) {
+    fourInfo.style.backgroundColor = "red"
+        } else {
+    fourInfo.style.backgroundColor = "gray"
+}
+
+// 5 pm
+if (now < 17) {
+    fiveInfo.style.backgroundColor = "green"
+    } else if (now == 17) {
+    fiveInfo.style.backgroundColor = "red"
+        } else {
+    fiveInfo.style.backgroundColor = "gray"
+}
+
     
-// 9 AM sav to local storage
+////// SAVE TO LOCAL STORAGE //////
+// 9 am
 nineInfo.value = localStorage.getItem("nine");
 
 $(save9).on("click", function(){
@@ -45,7 +121,8 @@ $(save9).on("click", function(){
     alert('Your event for 9 AM has been added!')
 })
 
-// 10 AM save to local storage
+
+// 10 am
 tenInfo.value = localStorage.getItem("ten");
 
 $(save10).on("click", function(){
@@ -53,7 +130,7 @@ $(save10).on("click", function(){
     alert('Your event for 10 AM has been added!')
 })
 
-// 11 AM save to local storage
+// 11 am
 elevenInfo.value = localStorage.getItem("eleven");
 
 $(save11).on("click", function(){
@@ -61,7 +138,7 @@ $(save11).on("click", function(){
     alert('Your event for 11 AM has been added!')
 })
 
-// 12 PM save to local storage
+// 12 pm
 twelveInfo.value = localStorage.getItem("twelve");
 
 $(save12).on("click", function(){
@@ -69,7 +146,7 @@ $(save12).on("click", function(){
     alert('Your event for 12 PM has been added!')
 })
 
-// 1 PM save to local storage
+// 1 pm
 oneInfo.value = localStorage.getItem("one");
 
 $(save1).on("click", function(){
@@ -77,7 +154,7 @@ $(save1).on("click", function(){
     alert('Your event for 1 PM has been added!')
 })
 
-// 2 PM save to local storage
+// 2 pm
 twoInfo.value = localStorage.getItem("two");
 
 $(save2).on("click", function(){
@@ -85,7 +162,7 @@ $(save2).on("click", function(){
     alert('Your event for 2 PM has been added!')
 })
 
-// 3 PM save to local storage
+// 3 pm
 threeInfo.value = localStorage.getItem("three");
 
 $(save3).on("click", function(){
@@ -93,7 +170,7 @@ $(save3).on("click", function(){
     alert('Your event for 3 PM has been added!')
 })
 
-// 4 PM save to local storage
+// 4 pm
 fourInfo.value = localStorage.getItem("four");
 
 $(save4).on("click", function(){
@@ -101,7 +178,7 @@ $(save4).on("click", function(){
     alert('Your event for 4 PM has been added!')
 })
 
-// 5 PM save to local storage
+// 5 pm
 fiveInfo.value = localStorage.getItem("five");
 
 $(save5).on("click", function(){
